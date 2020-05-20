2f1e8170bef46fecd4ef0601398f9995a728a5e1
Maxim Petrischak <petrischak.maxim@gmail.com>
2020-05-20 16:57:02 +0300

    добавил обработку столкновений мяча с блоками

--- arkanoid/game.js
+++ arkanoid/game.js
@@ -55,6 +55,8 @@ let game = {
         for (let row = 0; row < this.rows; row++) {
             for (let col = 0; col < this.cols; col++) {
                 this.blocks.push({
+                    width: 111,
+                    height: 39,
                     x: 113 * col + 70,
                     y: 42 * row + 90
                 });
@@ -64,6 +66,11 @@ let game = {
     update() {
         this.platform.move();
         this.ball.move();
+        for(let block of this.blocks) {
+           if( this.ball.collide(block)) {
+                this.ball.bumpBlock(block);
+           };
+        }
     },
     run() {
         window.requestAnimationFrame(() => {
@@ -115,7 +122,22 @@ game.ball = {
         if (this.dx) {
             this.x += this.dx;
         }
-    }
+    },
+    collide(element) {
+        let x = this.x + this.dx;
+        let y = this.y + this.dy;
+
+        if (x + this.width > element.x &&
+            x  < element.x + element.width &&
+            y + this.height > element.y &&
+            y < element.y + element.height) {
+                return true;
+            }
+        return false;
+    },
+    bumpBlock(block) {
+        this.dy *= -1;
+    },
 };
 
 game.platform = {
